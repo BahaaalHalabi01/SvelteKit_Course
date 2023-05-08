ec2c0ebd247c6022d4ea2833ebfb9600d8a30165
Bahaa al Halabi <67575710+BahaaalHalabi01@users.noreply.github.com>
2023-05-08 13:43:02 +0300

    tailwind + basic routing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,23 @@
 <script>
-	let title = 'SvelteKit Beginner Tutorial';
+	let title = 'SvelteKit Beginner Tutorial'
 
 	const updateTitle = () => {
-		title = 'A Changed Title on Click';
-	};
+		title = 'A Changed Title on Click'
+	}
 </script>
 
-<div class="index">
-	<h2>{title}</h2>
+<div class="text-center m-auto">
+	<h2 class="text-2xl font-bold my-4">{title}</h2>
 	<p>
 		Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit doloremque omnis adipisci
 		temporibus eos qui magnam aut unde officiis. Minus incidunt fugiat aliquam? Porro, facere itaque
 		nulla excepturi illo corrupti.
 	</p>
-	<p>
-		Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum quod officiis veritatis
-		accusamus cum. Modi, amet? Explicabo nostrum saepe molestiae ab ipsam inventore exercitationem,
-		voluptatem neque tenetur nesciunt at architecto.
-	</p>
-	<button on:click={updateTitle}>Change The Title</button>
-	<input type="text" bind:value={title}>
-</div>
+	<ul>
+		<li><a href="/about">About Us</a></li>
 
-<style>
-	.index {
-		text-align: center;
-		display: block;
-		margin: auto;
-	}
-</style>
+		<li><a href="/guides">Guides</a></li>
+	</ul>
+	<button on:click={updateTitle} class="my-4">Change The Title</button>
+	<input type="text" bind:value={title} class="border-solid border-black border-2 p-2" />
+</div>
---
+++ src/routes/guides/+page.svelte
@@ -0,0 +1,7 @@
+<div class="m-auto text-center">
+	<ul>
+		<li><a href="/">First Guide</a></li>
+		<li><a href="/">Second Guide</a></li>
+	</ul>
+</div>
+
