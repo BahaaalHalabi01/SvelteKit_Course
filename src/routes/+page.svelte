b7e48e4c7f582d469552e86a4dfe51786cab9082
Bahaa al Halabi <67575710+BahaaalHalabi01@users.noreply.github.com>
2023-05-08 03:30:45 +0300

    basic starting page changing title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let title = 'SvelteKit Beginner Tutorial';
+
+	const updateTitle = () => {
+		title = 'A Changed Title on Click';
+	};
+</script>
+
+<div class="index">
+	<h2>{title}</h2>
+	<p>
+		Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit doloremque omnis adipisci
+		temporibus eos qui magnam aut unde officiis. Minus incidunt fugiat aliquam? Porro, facere itaque
+		nulla excepturi illo corrupti.
+	</p>
+	<p>
+		Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum quod officiis veritatis
+		accusamus cum. Modi, amet? Explicabo nostrum saepe molestiae ab ipsam inventore exercitationem,
+		voluptatem neque tenetur nesciunt at architecto.
+	</p>
+	<button on:click={updateTitle}>Change The Title</button>
+	<input type="text" bind:value={title}>
+</div>
+
+<style>
+	.index {
+		text-align: center;
+		display: block;
+		margin: auto;
+	}
+</style>
