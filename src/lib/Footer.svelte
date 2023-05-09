0dd4944a32e1951361bdc727079dbfbc9c28ea40
Bahaa al Halabi <67575710+BahaaalHalabi01@users.noreply.github.com>
2023-05-10 01:02:48 +0300

    more styling

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,13 +1,13 @@
-<footer class="text-center mt-auto fixed bottom-0 w-full pb-4">
+<footer class="text-center mt-auto fixed bottom-0 w-full py-4 bg-zinc-800 text-white">
 	<p>
 		CopyRight 2023, Made Using <a
 			href="https://learn.svelte.dev"
 			target="_blank"
 			rel="noreferrer"
-			class=" text-white">SvelteKit</a
+			class=" text-svelte">SvelteKit</a
 		>
 		with the help of
-		<a href="https://youtube.com/@NetNinja" target="_blank" rel="noreferrer" class=" text-white"
+		<a href="https://youtube.com/@NetNinja" target="_blank" rel="noreferrer" class=" text-svelte"
 			>The Net Ninja's</a
 		> crash course.
 	</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+    extend: {
+      colors:{
+        'svelte':'#ff4700'
+      }
+
+    }
   },
   plugins: []
 }
