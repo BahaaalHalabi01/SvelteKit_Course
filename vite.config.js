8b23c58dd3999f4ee59087c7493edac06a9b411e
Bahaa al Halabi <67575710+BahaaalHalabi01@users.noreply.github.com>
2023-05-10 00:21:26 +0300

    new configs

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
+import { SvelteKitPWA } from '@vite-pwa/sveltekit'
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), SvelteKitPWA()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
